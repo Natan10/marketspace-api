36a5e61b1c1ef692031ca7f766cb879b22c4fe5f
Natan <natan-moreira1@hotmail.com>
2023-06-08 21:35:11 -0300

    adjust create script

--- db_scripts/create.sql
+++ db_scripts/create.sql
@@ -1,8 +1,8 @@
 create database workspace;
 
-drop table if exists users; 
-drop table if exists payment_methods; 
-drop table if exists announcements; 
+drop table if exists users 
+drop table if exists announcements 
+drop table if exists payment_methods
 
 create table if not exists users (
 	id serial primary key,
@@ -10,16 +10,7 @@ create table if not exists users (
 	email varchar(255) unique not null,
 	phone varchar(20) not null,
 	created_at timestamp not null default now()
-);
-
-create table if not exists payment_methods(
-	id serial primary key,
-	boleto bool default false,
-	pix bool default false,
-	cash bool default false,
-	credit_card bool default false,
-	bank_deposit bool default false
-);
+)
 
 create table if not exists announcements(
 	id serial primary key,
@@ -29,10 +20,20 @@ create table if not exists announcements(
 	price decimal,
 	is_exchangeable bool default false,
 	is_active bool default true,
-	users_id int not null,
-	payment_methods_id int not null,
-	
-	foreign key(users_id) references users(id) on delete cascade,
-	foreign key(payment_methods_id) references payment_methods(id)
-);
+	images varchar(255)[] default '{}',
+	user_id int not null,
+	foreign key(user_id) references users(id) on delete cascade
+)
+ 
+
+create table if not exists payment_methods(
+	id serial primary key,
+	boleto bool,
+	pix bool,
+	cash bool,
+	credit_card bool,
+	bank_deposit bool,
+	announcement_id int not null,
+	foreign key(announcement_id) references announcements(id) on delete cascade
+)
 
